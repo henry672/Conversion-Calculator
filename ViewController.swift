117029df07000f6008aa730e0159bf8cac68ee17
Henry Asare <henryasare@Henrys-MacBook-Pro.local>
2017-12-17 15:59:39 -0600

    Main storyboard created

--- ViewController.swift
+++ ViewController.swift
@@ -16,8 +16,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func numbers(_ sender: UIButton) {
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
